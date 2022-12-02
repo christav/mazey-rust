4be77435b5b3f5bdbd783e812daac048ac85a3aa
Chris Tavares <cct@tavaresstudios.com>
2022-12-01 21:02:23 -0800

    Command line switch for charset

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-extern crate rand;
-
+use std::env;
 mod mazey;
 use mazey::maze::Maze;
-use mazey::printer::{ AsciiCharSet, UnicodeCharSet, MazePrinter };
+use mazey::printer::{ MazeCharset, AsciiCharSet, UnicodeCharSet, MazePrinter };
+
 
 fn main() {
+    let char_set: &dyn MazeCharset;
+    let args: Vec<String> = env::args().collect();
+
+    char_set = if args.len() < 2 || args[1] == "-u" {
+        &UnicodeCharSet {}
+    } else {
+        &AsciiCharSet {}
+    };
+
     let maze = Maze::new(30, 20);
-    let printer1 = MazePrinter::new(&AsciiCharSet {});
-    let printer2 = MazePrinter::new(&UnicodeCharSet {});
+    let printer = MazePrinter::new(char_set);
 
-    printer1.print(&maze);
-
-    println!();
-
-    printer2.print(&maze);
+    printer.print(&maze);
 }
